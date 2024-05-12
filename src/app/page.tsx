c267ccfc2421028111b1e6f95f07738eab65dfef
jordy <jordy.10.garcia@gmail.com>
2024-05-12 02:27:50 -0300

    add: h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
-  return <p>hola</p>
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Marketplace {""}
+          <span className="text-blue-600">Cordoba</span>.
+        </h1>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
