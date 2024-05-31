6236ae8fc2c50390c803a7ade8885e6d854dc274
jordy <jordy.10.garcia@gmail.com>
2024-05-31 01:20:31 -0300

    add: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,27 @@
+import { Icons } from "./Icons";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+
 const Navbar = () => {
-  return <div className="bg-white sticky z-50"></div>;
+  return (
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-white">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              {/* {TODO: mobile nav} */}
+
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h10 w-10" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
 };
 
 export default Navbar;
